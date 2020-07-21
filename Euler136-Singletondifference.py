4f972c2dee4fe66794cc54e61fc85f5348cf30dd
miriamwaldvogel <miriamwaldvogel@penguin>
2020-07-21 08:33:34 -0700

    Removed debug, fixed output

--- Euler136-Singletondifference.py
+++ Euler136-Singletondifference.py
@@ -1,10 +1,9 @@
 def solutions(n):
     s = [0]*(n+1)
     for i in range(1, int(n/2)+1):
-        for j in range(1, int(n/i)+1):
+        for j in range(int(i/3), int(n/i)+1):
             if s[i*j] < 2 and 3*j > i and (i+j) % 4 == 0 and (3*i-j) % 4 == 0:
                 s[i*j] += 1
-    #print(sum(1 for i in s if i == 1))
-    print([i for i, j in enumerate(s) if j == 1])
+    print(sum(1 for i in s if i == 1))
 
-solutions(100)
+solutions(50000000)
